92d211277130de7c1f0dda4b551e3bdf43d98283
chlwhdtn03 <chlwhdtn03@naver.com>
2022-06-02 18:56:29 +0900

    라우터 메소드 작업

--- src/main/kotlin/server/PortableServer.kt
+++ src/main/kotlin/server/PortableServer.kt
@@ -1,17 +1,39 @@
 package server
 
+import data.PortableObject
+import data.RouterType
+import file.FileManager
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServer
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.Router
+import ui.Bootstrap
 
 class PortableServer(VERSION: String, PORT: Int) {
-    init {
+    companion object {
         val vertx: Vertx = Vertx.vertx()
         val server:HttpServer = vertx.createHttpServer()
         val router:Router = Router.router(vertx)
 
+        fun addRoute(path: String, target_object: PortableObject, type: RouterType) {
+            if(type == RouterType.GET || type == RouterType.GET_POST) {
+                router.get(path).handler { requesthandler ->
+                    val response = requesthandler.response()
+                    response.putHeader("content-type","text/plain")
+                    response.end("GET for $target_object")
+                }
+            }
+            if(type == RouterType.POST || type == RouterType.GET_POST) {
+                router.post(path).handler { requesthandler ->
+                    val response = requesthandler.response()
+                    response.putHeader("content-type","text/plain")
+                    response.end("POST for $target_object")
+                }
+            }
+        }
+    }
+    init {
         router.route("/").handler { requesthandler ->
             val response = requesthandler.response()
             response.putHeader("content-type", "text/plain")
@@ -28,10 +50,12 @@ class PortableServer(VERSION: String, PORT: Int) {
             """.trimIndent())
         }
 
-
         server.requestHandler(router).listen(PORT).onComplete {
             println("Server Opened in port $PORT")
             println("URL : http://localhost/")
         }
     }
+
+
+
 }
