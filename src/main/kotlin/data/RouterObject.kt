0ec1c9aaa4209ef6c55483e3f9d6f7879a92c646
최종수 <chlwhdtn03@naver.com>
2022-06-03 12:15:21 +0900

    RouterObject.kt 수정

--- src/main/kotlin/data/RouterObject.kt
+++ src/main/kotlin/data/RouterObject.kt
@@ -3,6 +3,13 @@ package data
 @kotlinx.serialization.Serializable
 data class RouterObject(
     var name: String, // 별명
-    var address: String, // 하위 주소
-    var type: RouterType // GET, POST, GET&POST
-)
+    val address: String, // 하위 주소
+    val type: RouterType, // GET, POST, GET&POST
+    val target_object: PortableObject? // 이거 Null이면 message에 꼭 값 필요함
+) {
+    var message:String = ""
+
+    constructor(name: String, address: String, type: RouterType, target_object: PortableObject?, message:String) : this(name, address, type, target_object) {
+        this.message = message
+    }
+}
